8cede51adbd70b2775c0ae66108b611d8dd3aae8
Nirodha97 <nirodhamwickramaratne@gamail.com>
2024-04-09 16:12:49 +0530

    Update url

--- src/employeeServices.js
+++ src/employeeServices.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const EMPLOYEE_API_BASE_URL = "http://employee-management-backend-2638160639:9090/employeeManagement/api/v1/employees"; 
+const EMPLOYEE_API_BASE_URL = "https://560ebe39-5f34-4ca7-92f5-86664940bfd5-dev.e1-us-east-azure.choreoapis.dev/employee-management-be/employee-management-backend/employee-management-backened-d2d/v1.0/api/v1/employees"; 
 
 class EmployeeService {
 
